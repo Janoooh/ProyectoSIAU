0ec4aaec4b08de71fbda4b89223a990ff30756a0
Delacrux <ninocordova1611@gmail.comj>
2025-05-30 19:41:26 -0400

    ver 0.25 "agrgue cosas "

---
+++ cmake-build-proyecto-siau/CMakeFiles/ProyectoSIAU.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProyectoSIAU.dir/main.c.obj"
+  "CMakeFiles/ProyectoSIAU.dir/main.c.obj.d"
+  "ProyectoSIAU.exe"
+  "ProyectoSIAU.exe.manifest"
+  "ProyectoSIAU.pdb"
+  "libProyectoSIAU.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProyectoSIAU.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
